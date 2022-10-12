7ccf3a271894ef479d3a821c111ffccc5f81b2b5
xingadora <74115064+xingadora@users.noreply.github.com>
2022-10-11 22:05:17 -0700

    add html to json converter

---
+++ src/htmltojson.py
@@ -0,0 +1,27 @@
+import json
+
+def html_to_json(content, indent=None):
+    soup = content
+    rows = soup.find_all("tr")
+
+    headers = {}
+    thead = soup.find("thead")
+    if thead:
+        thead = thead.find_all("th")
+        for i in range(len(thead)):
+            headers[i] = thead[i].text.strip().lower()
+    data = []
+    for row in rows:
+        cells = row.find_all("td")
+        if thead:
+            items = {}
+            if len(cells) > 0:
+                for index in headers:
+                    items[headers[index]] = cells[index].text
+        else:
+            items = []
+            for index in cells:
+                items.append(index.text.strip())
+        if items:
+            data.append(items)
+    return json.dumps(data, indent=indent)
