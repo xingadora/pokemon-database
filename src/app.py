93647936bde73fb11c110c04c2b32715cfa910b4
xingadora <74115064+xingadora@users.noreply.github.com>
2022-10-11 22:30:12 -0700

    fix encoding output

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup, NavigableString
 
 
-pokemon_name = 'bulbasaur' # change this to whatever pokemon you want
+pokemon_name = 'pikachu' # change this to whatever pokemon you want
 
 response = requests.get(
     f'https://pokemondb.net/pokedex/{pokemon_name}/moves/1')
@@ -15,6 +15,9 @@ table = soup.find("table")
 
 for element in table.find_all(string="—"):
     element.string.replace_with("none")
+
+for element in table.find_all(string="∞"):
+    element.string.replace_with("infinite")
 
 
 for element in table.find_all(attrs={"data-sort-value": "physical"}):
