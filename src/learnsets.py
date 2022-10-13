529eb4461cc3890a3d68bfd14913b75d6f7b7d20
Isaiah <isaiahc11@educbe.ca>
2022-10-12 20:19:31 -0600

    fixed output formatting

--- src/learnsets.py
+++ src/learnsets.py
@@ -3,39 +3,51 @@ import requests
 from bs4 import BeautifulSoup, NavigableString
 import json
 
-with open('/Users/isaiahchan/Documents/pokemon-learnsets/src/pokemonstats.json', 'r') as myfile:
-    data=myfile.read()
+
+with open('src\pokemonstats.json', 'r') as myfile:
+    data = myfile.read()
 
 pokemonlist = json.loads(data)
 
-print(pokemonlist[int("name")])
-
-pokemon_name = 'pikachu' # change this to whatever pokemon you want
-
-response = requests.get(
-    f'https://pokemondb.net/pokedex/{pokemon_name}/moves/1')
-
-soup = BeautifulSoup(response.text, 'html.parser')
-
-table = soup.find("table")
 
 
-for element in table.find_all(string="—"):
-    element.string.replace_with("none")
+def generate_learnset(pokemon_name):
+    response = requests.get(
+        f'https://pokemondb.net/pokedex/{pokemon_name}/moves/1')
 
-for element in table.find_all(string="∞"):
-    element.string.replace_with("infinite")
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    table = soup.find("table")
+
+    for element in table.find_all(string="—"):
+        element.string.replace_with("none")
+
+    for element in table.find_all(string="∞"):
+        element.string.replace_with("infinite")
+
+    for element in table.find_all(attrs={"data-sort-value": "physical"}):
+        element.insert(0, NavigableString("physical"))
+
+    for element in table.find_all(attrs={"data-sort-value": "special"}):
+        element.insert(0, NavigableString("special"))
+
+    for element in table.find_all(attrs={"data-sort-value": "status"}):
+        element.insert(0, NavigableString("status"))
+
+    with open("learnsets.json", "a") as o:
+        o.write('{"' + pokemon_name + '":')
+        o.write(html_to_json(table))
+        o.write('},')
 
 
-for element in table.find_all(attrs={"data-sort-value": "physical"}):
-    element.insert(0, NavigableString("physical"))
-
-for element in table.find_all(attrs={"data-sort-value": "special"}):
-    element.insert(0, NavigableString("special"))
-
-for element in table.find_all(attrs={"data-sort-value": "status"}):
-    element.insert(0, NavigableString("status"))
 
 
-with open("randomfile.json", "a") as o:
-    o.write(html_to_json(table))
+with open("learnsets.json", "a") as o:
+    o.write("[")
+
+for pokemon in pokemonlist:
+    pokemon_name = pokemon['name']
+    generate_learnset(pokemon_name)
+
+with open("learnsets.json", "a") as o:
+    o.write("{" + "}]")
