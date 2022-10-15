bbf3e9e681d615e3d77580be7e84066dade8f4b0
Isaiah <isaiahc11@educbe.ca>
2022-10-14 19:54:41 -0600

    reconfigure functions

---
+++ src/functions/generatestats.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup, NavigableString
+from htmltojson import *
+
+
+def generate_stats(outputFile):
+    link = 'https://pokemondb.net/pokedex/all'
+    response = requests.get(link)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    table = soup.find("table")
+
+    with open(outputFile, "a") as o:
+        o.write(html_to_json(table))
+
+
+
+
+
+
+print(a)
